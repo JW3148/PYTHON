f4472afa2fe4c60e5855e80d79d542d8cd725f83
JW3148 <jhw3148@gmail.com>
2016-05-15 00:16:11 -0400

    completed exercise string.py

---
+++ strings.py
@@ -0,0 +1,58 @@
+'''
+Edit this file to complete the exercises in
+'Print to Screen and Transform Strings'
+'''
+
+#Just to get you going, here's the answer to
+#the first exercise:
+print("Change Banking For Good.")
+
+import math
+e = math.e
+#Now print "e to 10 decimal places: ", followed by the
+#numerical value of e out to 10 decimal places, using
+#string formatting and e, defined above.
+print('e to 10 decimals:{0:.10f}'.format(e))
+#e to 10 decimals:2.7182818285
+
+mixed_case_string = "cHAngE BANKing fOR GooD."
+#Set lower_case_string equal to mixed_case_string, only
+#with all lower case letters.
+lower_case_string = mixed_case_string.lower()
+print (lower_case_string)
+
+print("Isthis string lower case? {0:s}".format(lower_case_string))
+
+
+#The first paragraph of Capital One's Wikipedia article
+#(Accessed 4/20/2015):
+wiki_intro = "Capital One Financial Corporation is a U.S."\
+  "-based bank holding company specializing in credit cards,"\
+  " home loans, auto loans, banking and savings products. When "\
+  "measured in terms of total assets and deposits, Capital One is "\
+  "the eighth largest bank holding company in the United States. "\
+  "As of 2012, The bank has 963 Capital One Bank Branches including "\
+  "10 cafe style locations for their Capital One 360 brand and 2,000 "\
+  "ATMs. Capital One Financial is a member of the Fortune 500, and "\
+  "also conducts business in Canada and the United Kingdom. The "\
+  "company helped pioneer the mass marketing of credit cards in "\
+  "the 1990s, and it is now the fourth-largest customer of the "\
+  "United States Postal Service."
+
+print(wiki_intro)
+#set num_branches to equal the 311th through 313th characters
+#of the wiki_intro string. Note that wiki_intro uses linebreaks
+#(backslashes \) to make this long string easier to read. The
+#linebreaks don't count as a character in the string.
+num_branches = wiki_intro[310:313]
+print(num_branches)
+
+
+
+
+#set wiki_intro_nospaces to equal wiki_intro, but with *ALL* of
+#the spaces removed - e.g. "Capital One Financial Corporation"
+#becomes "CapitalOneFinancialCorporation".
+wiki_intro_nospaces = wiki_intro.replace(' ','')
+print(wiki_intro_nospaces)
+
