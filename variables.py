c5404aa7d5f002954bdcddfc43ad6c9512516b21
JW3148 <jhw3148@gmail.com>
2016-05-14 20:20:07 -0400

    Exercise variable.py

---
+++ variables.py
@@ -0,0 +1,22 @@
+'''
+Edit this file to contain the variable assignments.
+To get you started the first one is done for you.
+The quotes are necessary for storing text in a
+variable, but not for numbers.
+'''
+
+
+
+
+name = "Lee Cardholder" 
+
+balance = 120.50
+
+percent_apr = 20
+
+from fractions import Fraction
+monthly_interest = balance * Fraction(percent_apr,100)
+print monthly_interest
+
+
+
